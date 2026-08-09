1beb0f78a682fd580029535d4f5dcec8c83aba0d
thifathul <andrianmuhamad270@gmail.com>
2026-08-09 11:56:44 +0700

    bypass

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,20 @@ Route::get('/dashboard', function () {
     }
 })->middleware(['auth'])->name('dashboard');
 
+// Bypassing Symlink for Shared Hosting
+Route::get('/storage/{folder}/{filename}', function ($folder, $filename) {
+    $path = storage_path('app/public/' . $folder . '/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    $file = file_get_contents($path);
+    $type = mime_content_type($path);
+
+    return response($file, 200)->header('Content-Type', $type);
+})->where('filename', '.*');
+
 // Master Dashboard Group
 Route::middleware(['auth', 'role:master'])->group(function () {
     Route::get('/master/dashboard', function () {
